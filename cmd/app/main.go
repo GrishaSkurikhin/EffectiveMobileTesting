b8d2ad5733c77a677d9ba69b22e1d39eb8cf1354
GrishaSkurikhin <grishaskurihin1@gmail.com>
2023-09-14 01:57:37 +0300

    add logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,68 @@
 package main
 
 import (
+	"os"
+
 	"github.com/GrishaSkurikhin/EffectiveMobileTesting/internal/config"
+	"github.com/GrishaSkurikhin/EffectiveMobileTesting/internal/lib/logger/slogpretty"
+	"golang.org/x/exp/slog"
 )
 
-const configPath = "config/.env"
+const (
+	configPath = "config/.env"
+
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func init() {
 	config.MustLoad(configPath)
 }
 
 func main() {
+	cfg := config.New()
 
+	log := setupLogger(cfg.Env)
+	log.Info(
+		"starting app",
+		slog.String("env", cfg.Env),
+		slog.String("version", "1"),
+	)
+	log.Debug("debug messages are enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/GrishaSkurikhin/EffectiveMobileTesting
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
